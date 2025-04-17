90537cc2ee2674694b383930cfbc09bd895fd614
Dion Cosina <dion.cosina@hva.nl>
2025-04-17 12:41:09 +0200

    feat: made a type for Game

---
+++ src/api/src/types/Game.ts
@@ -0,0 +1,11 @@
+export interface Game {
+    id: number;
+    title: string;
+    thumbnail: string;
+    descriptionMarkdown: string;
+    descriptionHtml: string;
+    url: string;
+    images: string;
+    authors: string;
+    tags: string;
+}

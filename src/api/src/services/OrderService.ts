fb02b516321ec9a70e1ea7f239e2ee51f756f91e
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-22 16:20:54 +0200

    Verbinding maken met database en een async toevoegen

--- src/api/src/services/OrderService.ts
+++ src/api/src/services/OrderService.ts
@@ -1,2 +1,15 @@
 import { DatabaseService } from "./DatabaseService";
 import { PoolConnection } from "mysql2";
+
+export class OrderService {
+    private readonly _db: DatabaseService = new DatabaseService();
+
+    public async createOrder(
+        sessionId: string,
+        orderNumber: string,
+        totalPrice: number
+    ): Promise<number> {
+        const connection: PoolConnection = await this._db.openConnection();
+    }
+
+}

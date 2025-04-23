cd67f878f18b7b6ed661b317751b56f4255ef953
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-23 18:34:22 +0200

    async fuctie voor seesion id, ordernummer en prijs aangemaakt en begonnen aan fout melding

--- src/api/src/controllers/OrderContoller.ts
+++ src/api/src/controllers/OrderContoller.ts
@@ -3,4 +3,15 @@ import { OrderService } from "@api/services/OrderService";
 
 export class OrderController {
     private readonly _orderService: OrderService = new OrderService();
+
+    public async completeOrder(req: Request, res: Response): Promise<void> {
+        const sessionId = req.sessionId!;
+        const { orderNumber, totalPrice } = req.body;
+    }
+
+    if (!orderNumber || totalPrice === undefined) {
+      res.status(400).json({ error: "Missing orderNumber or totalPrice" });
+      return;
+        
+    } 
 }

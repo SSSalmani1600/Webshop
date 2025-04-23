e4f91babbca3e7a8447d87511133fd376b912f95
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-23 18:21:52 +0200

    export class toegevoegd

--- src/api/src/controllers/OrderContoller.ts
+++ src/api/src/controllers/OrderContoller.ts
@@ -1,2 +1,6 @@
 import { Request,Response } from "express";
 import { OrderService } from "@api/services/OrderService";
+
+export class OrderController {
+    private readonly _orderService: OrderService = new OrderService();
+}

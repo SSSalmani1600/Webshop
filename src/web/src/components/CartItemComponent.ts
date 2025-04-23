7b43474e5674aca4b375fe8575464a98041ca6e8
KevinKhd <kevinkhd03@gmail.com>
2025-04-24 01:08:42 +0200

    styling voor winkelwagen voor nu af

--- src/web/src/components/CartItemComponent.ts
+++ src/web/src/components/CartItemComponent.ts
@@ -1,23 +1,22 @@
 import { CartItem } from "../interfaces/CartItem";
 
 export class CartItemComponent extends HTMLElement {
-  constructor(item: CartItem) {
-    super();
+    public constructor(item: CartItem) {
+        super();
 
-    const price = typeof item.price === "string" ? parseFloat(item.price) : item.price;
+        const price: number = typeof item.price === "string" ? parseFloat(item.price) : item.price;
 
-    this.innerHTML = `
+        this.innerHTML = `
       <div class="cart-item">
         <img src="${item.thumbnail}" alt="${item.title}">
         <div>
           <h4>${item.title}</h4>
           <p>Prijs: €${price.toFixed(2)}</p>
           <p>Aantal: ${item.quantity}</p>
-          <button disabled>Remove item</button>
         </div>
       </div>
     `;
-  }
+    }
 }
 
 customElements.define("cart-item", CartItemComponent);

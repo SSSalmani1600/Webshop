02310b1e2606514f9230b11893c1b474b8072bac
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-24 10:35:20 +0200

    nieuwe verbetering voor order controller

--- src/api/src/controllers/OrderContoller.ts
+++ src/api/src/controllers/OrderContoller.ts
@@ -3,14 +3,17 @@ import { OrderService } from "@api/services/OrderService";
 
 const orderService = new OrderService();
 
-function getSessionIdFromCookies(req: Request): string | null {
-    const cookie = req.headers.cookie;
-    if (!cookie) return null;
-
-    const match = cookie.match(/session=([^;]+)/);
-    return match ? match[1] : null;
-}
-
 export class OrderController {
-    
+    public async createOrder(req: Request, res: Response): Promise<void> {
+        try {
+            const sessionId = req.sessionId;
+
+            if (!sessionId) {
+                res.status(401).json({ error: "Geen geldige sessie" });
+                return;
+            }
+        
+        }
+    }
+
 }

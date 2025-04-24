2f20267c38168f82ec9084d4ef0352aae50a105a
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-24 10:26:49 +0200

    cookie checker gemaakt

--- src/api/src/controllers/OrderContoller.ts
+++ src/api/src/controllers/OrderContoller.ts
@@ -1,3 +1,16 @@
 import { Request, Response } from "express";
 import { OrderService } from "@api/services/OrderService";
 
+const orderService = new OrderService();
+
+function getSessionIdFromCookies(req: Request): string | null {
+    const cookie = req.headers.cookie;
+    if (!cookie) return null;
+
+    const match = cookie.match(/session=([^;]+)/);
+    return match ? match[1] : null;
+}
+
+export class OrderController {
+    
+}

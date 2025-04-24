eba812bdda7df4803b92018ff98294e8be609c1f
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-04-24 10:20:31 +0200

    problemen fixen

--- src/api/src/controllers/OrderContoller.ts
+++ src/api/src/controllers/OrderContoller.ts
@@ -1,18 +1,3 @@
 import { Request, Response } from "express";
 import { OrderService } from "@api/services/OrderService";
-import { sessionMiddleware } from "@api/middleware/sessionMiddleware";
 
-export class OrderController {
-    private readonly _orderService: OrderService = new OrderService();
-
-    public async completeOrder(req: Request, res: Response): Promise<void> {
-        const sessionId = req.sessionId!;
-        const { orderNumber, totalPrice } = req.body;
-    }
-
-    if (!orderNumber || totalPrice === undefined) {
-      res.status(400).json({ error: "Ordernummer of totaalprijs ontbreekt" });
-      return;  
-      
-    } 
-}

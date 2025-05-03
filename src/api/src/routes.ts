55a604c497b322ead34c3e52329e62845aa618de
Nabil Salmani <nabil.salmani@hva.nl>
2025-05-03 19:32:24 +0200

    updated code

--- src/api/src/routes.ts
+++ src/api/src/routes.ts
@@ -7,20 +7,16 @@ import CheckoutController from "./controllers/CheckoutController";
 
 
 
-// maak a router
 export const router: Router = Router();
 
-// Setup endpoints
 router.get("/", (_, res) => {
     res.send("Welcome to the API!");
 });
 
-// Forward endpoints to other routers
 const welcomeController: WelcomeController = new WelcomeController();
 const cartController: CartController = new CartController();
 const productController: ProductController = new ProductController();
 
-// NOTE: After this line, all endpoints will check for a session.
 router.use(sessionMiddleware);
 
 router.get("/session", (req, res) => welcomeController.getSession(req, res));
@@ -29,12 +25,10 @@ router.delete("/session/expired", (req, res) => welcomeController.deleteExpiredS
 router.get("/welcome", (req, res) => welcomeController.getWelcome(req, res));
 router.get("/cart", (_req, _res) => cartController.getCart(_req, _res));
 
-// NOTE: After this line, all endpoints will require a valid session.
 router.use(requireValidSessionMiddleware);
 
 router.get("/secret", (req, res) => welcomeController.getSecret(req, res));
 
-// TODO: The following endpoints have to be implemented in their own respective controller
 router.get("/products", (_req, _res) => productController.getAllGames(_req, _res));
 router.get("/product-prices/:id", (req, res) => productController.getGamePrice(req, res));
 

81b2804dc566281150f23e2393b86fdebda7aefe
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-12 15:03:10 +0200

    verandering service

--- src/api/src/services/ProductDetailService.ts
+++ src/api/src/services/ProductDetailService.ts
@@ -1,28 +1,31 @@
 import { DatabaseService } from "./DatabaseService";
 import { PoolConnection } from "mysql2/promise";
+import { Game } from "@api/types/Game";
 
 export class GameService {
     private readonly _db: DatabaseService = new DatabaseService();
 
-    public async getGameById(gameId: string): Promise<{
-        id: number;
-        title: string;
-        thumbnail: string;
-        descriptionHtml: string;
-    } | null> {
+    public async getGameById(gameId: string): Promise<Game | null> {
         const connection: PoolConnection = await this._db.openConnection();
 
         try {
-            const [rows] = await connection.query(
-                `
-                SELECT id, title, thumbnail, descriptionHtml
-                FROM games
-                WHERE id = ?
-                `,
-                [gameId]
-
+            const [rows] = await connection.query<Game[]>(
+            `
+            SELECT id, title, thumbnail, descriptionHtml
+            FROM games
+            WHERE id = ?
+            `,
+            [gameId]
             );
-        }
 
+            const game: Game | undefined = rows[0];
+            return game || null;
+        }
+        catch (e) {
+            throw new Error(`Kan game niet ophalen: ${e}`);
+        }
+        finally {
+            connection.release();
+        }
     }
 }

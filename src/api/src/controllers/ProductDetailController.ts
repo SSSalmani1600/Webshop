6572e2b553d3e1e8ab6246545088a6aa18e2750e
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-15 13:55:09 +0200

    if statement bij gameId

--- src/api/src/controllers/ProductDetailController.ts
+++ src/api/src/controllers/ProductDetailController.ts
@@ -2,11 +2,16 @@ import { Request, Response } from "express";
 import { GameDetailService } from "@api/services/ProductDetailService";
 import { Game } from "@api/types/Game";
 
+
 export class GameDetailService {
     private readonly _gameDetailService: GameDetailService = new GameDetailService();
 
     public async getGameById(req: Request, res: Response): Promise<void> {
         const gameId = req.query.id as string; 
+
+        if (!gameId) {
+           res.status(400).json({ error: "Game ID ontbreekt."})
+        }
 
         try {
 

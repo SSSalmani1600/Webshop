be2f2ffd768245abae3bae8395cd045b3d2a3c22
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-15 14:14:09 +0200

    controller af van gamedetail

--- src/api/src/controllers/ProductDetailController.ts
+++ src/api/src/controllers/ProductDetailController.ts
@@ -2,25 +2,30 @@ import { Request, Response } from "express";
 import { GameDetailService } from "@api/services/ProductDetailService";
 import { Game } from "@api/types/Game";
 
-
 export class GameDetailController {
     private readonly _gameDetailService: GameDetailService = new GameDetailService();
 
     public async getGameById(req: Request, res: Response): Promise<void> {
-        const gameId = req.query.id as string; 
+        const gameId: string = req.query.id as string;
 
         if (!gameId) {
-           res.status(400).json({ error: "Game ID ontbreekt."})
-           return;
+            res.status(400).json({ error: "Game ID ontbreekt." });
+            return;
         }
 
-        try { 
+        try {
             const game: Game | null = await this._gameDetailService.getGameById(gameId);
 
-            if (!game) { 
-                res.status(404).json({error: "Game niet gevonden"})
-                return;               
+            if (!game) {
+                res.status(404).json({ error: "Game niet gevonden" });
+                return;
             }
+
+            res.json(game);
+        }
+        catch (error) {
+            console.error("Fout bij ophalen van game:", error);
+            res.status(500).json({ error: "Kon game niet ophalen." });
         }
     }
 }

eed961ce211da3d4e2e2e86c177bacbae6c7344d
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-15 13:51:52 +0200

    import commit

--- src/api/src/controllers/ProductDetailController.ts
+++ src/api/src/controllers/ProductDetailController.ts
@@ -1,7 +1,15 @@
+import { Request, Response } from "express";
+import { GameDetailService } from "@api/services/ProductDetailService";
+import { Game } from "@api/types/Game";
+
 export class GameDetailService {
     private readonly _gameDetailService: GameDetailService = new GameDetailService();
 
-    public async (params:type) => {
-        
+    public async getGameById(req: Request, res: Response): Promise<void> {
+        const gameId = req.query.id as string; 
+
+        try {
+
+        }
     }
 }

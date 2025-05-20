4eae470a1374be0cf0fd4783643d556227158f33
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-20 13:56:15 +0200

    changes paths of order complete

---
+++ src/web/src/components/OrderCompleteComponent.ts
@@ -0,0 +1,25 @@
+import "@web/components/OrderConfirmationComponent";
+import { html } from "@web/helpers/webComponents";
+
+export class OrderCompletePageComponent extends HTMLElement {
+    public connectedCallback(): void {
+        this.attachShadow({ mode: "open" });
+        this.render();
+    }
+
+    private render(): void {
+        if (!this.shadowRoot) return;
+
+        const element: HTMLElement = html`
+      <div>
+        <h1>Bedankt voor je bestelling</h1>
+        <order-confirmation></order-confirmation>
+      </div>
+    `;
+
+        this.shadowRoot.firstChild?.remove();
+        this.shadowRoot.append(element);
+    }
+}
+
+window.customElements.define("webshop-page-order-complete", OrderCompletePageComponent);

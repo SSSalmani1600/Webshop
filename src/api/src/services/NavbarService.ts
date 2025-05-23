367512242444130d2c17a4662d5df0fd90adf404
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-23 16:40:03 +0200

    connection met database en controller zetten met if

--- src/api/src/services/NavbarService.ts
+++ src/api/src/services/NavbarService.ts
@@ -1,3 +1,27 @@
 import { DatabaseService } from "./DatabaseService";
 import { PoolConnection } from "mysql2/promise";
 
+export class NavbarService {
+    private readonly _db: DatabaseService = new DatabaseService();
+
+    public async getCartItemCount(userId: number): Promise<number> {
+        const connection: PoolConnection = await this._db.openConnection();
+        
+        try {
+            const [rows] = await connection.query(
+                `
+                SELECT COUNT(*) AS count
+                FROM cart
+                WHERE user_id = ?
+                `,
+                [userId]
+            );
+
+            if (!Array.isArray(rows) || rows.length === 0) {
+                return 0;
+            }
+
+            const count
+        }
+    }
+}

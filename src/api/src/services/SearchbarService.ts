16c36c4a1a376463b793a7e14eb95a82f66d3666
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-28 18:38:05 +0200

    connectie met de database select

--- src/api/src/services/SearchbarService.ts
+++ src/api/src/services/SearchbarService.ts
@@ -2,3 +2,24 @@ import { DatabaseService } from "./DatabaseService";
 import { PoolConnection } from "mysql2/promise";
 import { Game } from "@api/types/Game";
 
+export class GameSearchService {
+    private readonly _db: DatabaseService = new DatabaseService();
+
+    public async searchGamesByTitle(query: string): Promise<Game[]> {
+        const connection: PoolConnection = await this._db.openConnection();
+
+        try {
+            const [rows] = await connection.query(
+                `
+                SELECT id, title, thumbnail, descriptionHtml
+                FROM games
+                WHERE hidden = 0 AND title LIKE ?
+                `,
+                [`%${query}%`]
+            );
+
+            return rows as Game[];
+        }
+    }
+    
+}

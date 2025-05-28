1b883a8dc64768abc6a880f56b8363ee38a140de
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-28 19:05:12 +0200

    begonnen aan searchbar component

--- src/web/src/components/SearchbarComponent.ts
+++ src/web/src/components/SearchbarComponent.ts
@@ -1,2 +1,25 @@
 import type { Game } from "@api/types/Game";
 import { SessionResponse } from "@shared/types";
+
+export class GameSearchComponent extends HTMLElement {
+    private shadow: ShadowRoot;
+
+    public constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: "open" });
+    }
+
+    public connectedCallback(): void {
+        this.renderLoading();
+        void this.loadSearchResults();
+    }
+
+    private renderLoading(): void {
+        this.shadow.innerHTML = "<p style="color: white;">Zoekresultaten worden geladen...</p>";
+    }
+
+    private async loadSearchResults(): Promise<void> {
+        const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+        const query: string | null = urlParams.get("query");
+    }
+}

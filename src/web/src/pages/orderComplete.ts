ffe97e0240656b038fa593fbd0062fd28f008fc5
Nedzhat Nedzhat <nedzhat.nedzhat@hva.nl>
2025-05-30 19:53:03 +0200

    order ts aanpassing

--- src/web/src/pages/orderComplete.ts
+++ src/web/src/pages/orderComplete.ts
@@ -1,4 +1,5 @@
 import "@web/components/OrderCompleteComponent";
+import "@web/components/NavbarComponent";
 import { html } from "@web/helpers/webComponents";
 
 export class OrderCompletePageComponent extends HTMLElement {
@@ -12,12 +13,11 @@ export class OrderCompletePageComponent extends HTMLElement {
 
         const element: HTMLElement = html`
       <div>
+        <navbar-component></navbar-component>
         <h1>Bedankt voor je bestelling</h1>
         <order-confirmation></order-confirmation>
       </div>
     `;
-
-        this.shadowRoot.firstChild?.remove();
         this.shadowRoot.append(element);
     }
 }
